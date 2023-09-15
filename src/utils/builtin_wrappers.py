0a478aea0fd6d8f278c4a7da2a0cc4f3692fd8d9
BenceKanyari <123818102+BenceKanyari@users.noreply.github.com>
2023-09-15 14:01:38 +0200

    first try

---
+++ src/utils/builtin_wrappers.py
@@ -0,0 +1,11 @@
+
+def contains_values(input_list, element):
+    return element in input_list
+def number_of_elements_in_list(input_list):
+    return len(input_list)
+
+def remove_every_element_from_list(input_list):
+    input_list.clear()
+
+def reverse_list(input_list):
+    return input_list[::-1]
