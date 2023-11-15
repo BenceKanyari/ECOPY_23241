f0aded8efc90cc0ce50288da0f0cfe75c3dbdd25
BenceKanyari <123818102+BenceKanyari@users.noreply.github.com>
2023-11-15 20:36:20 +0100

    weekly test 8 (NHECBE)

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -87,3 +87,66 @@ class LinearRegressionNP:
         ars = self.ars
 
         return f'Centered R-squared: {crs:.3f}, Adjusted R-squared: {ars:.3f}'
+
+
+class LinearRegressionGLS:
+    def __init__(self, left_hand_side, right_hand_side):
+        self.left_hand_side = left_hand_side
+        self.right_hand_side = right_hand_side
+        self.coefficients = None
+        self.residuals = None
+        self.p_values = None
+        self.crs = None
+        self.ars = None
+        self.V_inv = None
+
+    def fit(self):
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        coef_as = np.linalg.inv(X.T @ X) @ X.T @ y
+
+        res_as = y - X @ coef_as
+        log_res_sq_as = np.log(res_as**2)
+
+        coef_as2 = np.linalg.inv(X.T @ X) @ X.T @ log_res_sq_as
+
+        self.V_inv = np.diag(1 / np.sqrt(np.exp(X @ coef_as2)))
+
+        self.coefficients = np.linalg.inv(X.T @ self.V_inv @ X) @ (X.T @ self.V_inv @ y)
+
+        n, k = X.shape
+        self.residuals = y - X @ self.coefficients
+        sigma_squared = np.sum(self.residuals ** 2) / (n - k)
+        var_beta = np.linalg.inv(X.T @ self.V_inv @ X) * sigma_squared
+        t_statistic = self.coefficients / np.sqrt(np.diag(var_beta))
+        self.p_values = 2 * (1 - stats.t.cdf(np.abs(t_statistic), df=n-k))
+
+        rss = y.T @ self.V_inv @ X @ np.linalg.inv(X.T @ self.V_inv @ X) @ X.T @ self.V_inv @ y
+        tss = y.T @ self.V_inv @ y
+
+        self.ars = 1 - (rss / (n - k)) / (tss / (n - 1))
+    def get_params(self):
+        return pd.Series(self.coefficients, name='Beta coefficients')
+
+    def get_pvalues(self):
+        return pd.Series(self.p_values, name='P-values for the corresponding coefficients')
+
+    def get_wald_test_result(self, constraints):
+        constraints = np.array(constraints)
+        n = len(self.left_hand_side)
+        m, k = constraints.shape
+        sigma_squared = np.sum(self.residuals ** 2) / (n - k)
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+
+        H = constraints @ np.linalg.inv(X.T @ self.V_inv @ X) @ constraints.T
+        wald = (constraints @ self.coefficients).T @ np.linalg.inv(H) @ (constraints @ self.coefficients)
+        wald = wald / m / sigma_squared
+        p_value = 1 - stats.f.cdf(wald, dfn=m, dfd=n-k)
+
+        return f'Wald: {wald:.3f}, p-value: {p_value:.3f}'
+
+    def get_model_goodness_values(self):
+        crs = self.crs
+        ars = self.ars
+
+        return f'Centered R-squared: {crs:.3f}, Adjusted R-squared: {ars:.3f}'
